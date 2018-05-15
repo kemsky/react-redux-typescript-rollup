92ab6c43742545419b75a626ce07300c1117d1df
kemsky <nimrod97@gmail.com>
2018-05-15 04:00:13 +0300

    initial

--- src/state/application/actions.ts
+++ src/state/application/actions.ts
@@ -1,20 +1,13 @@
+import { actionCreator } from '../actions.common';
+
 export enum ApplicationActions
 {
     SimpleAction1 = 'SimpleAction1',
-    SimpleAction2 = 'SimpleAction2',
+    SimpleAction2 = 'SimpleAction2'
 }
 
-export const SimpleAction2 = 'SimpleAction2';
+export const simpleAction1 = actionCreator<ApplicationActions.SimpleAction1, number>(ApplicationActions.SimpleAction1);
 
-export const simpleAction1 = (payload: number) => ({
-    type   : ApplicationActions.SimpleAction1 as typeof ApplicationActions.SimpleAction1,
-    payload: payload
-});
-
-export const simpleAction2 = (payload: string) => ({
-    type   : ApplicationActions.SimpleAction2 as typeof ApplicationActions.SimpleAction2,
-    payload: payload
-});
-
+export const simpleAction2 = actionCreator<ApplicationActions.SimpleAction2, string>(ApplicationActions.SimpleAction2);
 
 export type ApplicationActionType = ReturnType<typeof simpleAction1> | ReturnType<typeof simpleAction2>;
