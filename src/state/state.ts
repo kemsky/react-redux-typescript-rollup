c78fef78d5a3bdbc50c327459257e468d6bc7f1a
kemsky <nimrod97@gmail.com>
2018-05-15 03:07:25 +0300

    initial

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,4 +1,4 @@
-import { combineReducers, createStore, Store } from 'redux';
+import { createStore, Store } from 'redux';
 import { ApplicationReducer } from './application/reducers';
 
-export const store: Store<IState> = createStore(combineReducers(ApplicationReducer), {message: 'Hello 123!'});
+export const store: Store<IState> = createStore(ApplicationReducer, {message: 'Hello 123!'});
