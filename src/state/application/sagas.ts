bc8959715bf0706f8e22e2ceb47035b6f81a78c9
kemsky <nimrod97@gmail.com>
2018-05-18 23:04:23 +0300

    add axios call

--- src/state/application/sagas.ts
+++ src/state/application/sagas.ts
@@ -2,17 +2,22 @@ import { put, takeLatest } from 'redux-saga/effects';
 import { ApplicationActions, ApplicationActionTypes } from './actions';
 import axios from 'axios';
 import { promise } from '../common/sagas';
-import { Dispatch } from 'react-redux';
 
-export function* Saga1(this:{dispatch: Dispatch})
+interface IPostResult
+{
+    data: { userId: number, id: number, title: string, body: string };
+}
+
+export function* Saga1()
 {
     //todo: no typing here, see https://github.com/Microsoft/TypeScript/issues/2983
-    const result = yield promise(() => axios.get<{data:{userId:number, id:number, title:string, body:string}}>('https://jsonplaceholder.typicode.com/posts/1'));
+    const result:IPostResult = yield promise(() => axios.get<IPostResult>('https://jsonplaceholder.typicode.com/posts/1'));
 
     yield put(ApplicationActions.createAction4(JSON.stringify(result.data)));
 }
 
 
-export function* Saga1Watcher() {
+export function* Saga1Watcher()
+{
     yield takeLatest(ApplicationActionTypes.Action3, Saga1);
 }
